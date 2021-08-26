0c062bb2a93f0fb41c2898eb629cd317c410e165
Singh-Parmeet <89386812+Singh-Parmeet@users.noreply.github.com>
2021-08-26 20:49:52 +0530

    Update diamond.js

--- extra/pattern/diamond.js
+++ extra/pattern/diamond.js
@@ -1,30 +1,30 @@
-function createDimondShape(size){
-    for(var i=1;i<=size;i++){
-       for(var s=size-1;s>=i;s--){
-          process.stdout.write(" ");
-       }
-       for(var j=1;j<=i;j++){
-          process.stdout.write("* ")
-       }
-       console.log();
-    }
-    if(i==size+1){
-       for(var i=1;i<=size-1;i++){
-          for(var s=1;s<=i;s++){
-             process.stdout.write(" ");
-          }
-          for(j=i;j<=size-1;j++){
-             process.stdout.write("* ");
-          }
-          console.log();
-       }
-    }
-  }
-  const size = process.argv.slice(2);
+function createDiamondShape(size){
+   for(var i=1;i<=size;i++){
+      for(var s=size-1;s>=i;s--){
+         process.stdout.write(" ");
+      }
+      for(var j=1;j<=i;j++){
+         process.stdout.write("* ")
+      }
+      console.log();
+   }
+   if(i==size+1){
+      for(var i=1;i<=size-1;i++){
+         for(var s=1;s<=i;s++){
+            process.stdout.write(" ");
+         }
+         for(j=i;j<=size-1;j++){
+            process.stdout.write("* ");
+         }
+         console.log();
+      }
+   }
+ }
+ const size = process.argv.slice(2);
 a = Number(size);
 if (a<11 && a>1){
-    createDiamondShape(a);
+   createDiamondShape(a);
 }
 else{
-    console.log("Please enter number between 2 to 10")
+   console.log("Please enter number between 2 to 10")
 }
