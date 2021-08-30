aef4482b2fc8157b4cb7242dd441ba85cd75b061
Singh-Parmeet <parmeet.singh@successive.tech>
2021-08-30 18:39:45 +0530

    feat: feature/#39519 add constants.js and index.js

---
+++ extra/constants.js
@@ -0,0 +1,18 @@
+const permission ={
+  getUsers: {
+  all: ['head-trainer'],
+  read : ['trainee', 'trainer'],
+  write : ['trainer'],
+  delete: [],
+}
+
+}
+
+const { getUsers }  = permission;
+
+const users = [{
+  traineeEmail: 'trainee1@successive.tech',
+  reviewerEmail: 'reviewer1@successive.tech',
+}]
+
+export {getUsers, users};
