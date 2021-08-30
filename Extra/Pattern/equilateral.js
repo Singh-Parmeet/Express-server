9274f39b19c9e202a2f9d63d4cac2002dc934fce
Singh-Parmeet <parmeet.singh@successive.tech>
2021-08-30 12:16:28 +0530

    make changes in spacing

--- Extra/Pattern/equilateral.js
+++ Extra/Pattern/equilateral.js
@@ -1,13 +1,19 @@
 function createEquilateral(size){
-    for(var i=1;i<=size;i++){
-        for(var s=size-1;s>=i;s--){
-           process.stdout.write(" ");
-        }
-        for(var j=1;j<=i;j++){
-           process.stdout.write("* ")
-        }
-        console.log();
-     }
+   for(var i=1;i<=size;i++){
+       for(var s=size-1;s>=i;s--){
+          process.stdout.write(" ");
+       }
+       for(var j=1;j<=i;j++){
+          process.stdout.write("* ")
+       }
+       console.log();
+    }
 } 
-const size = process.argv.slice(2);
-createEquilateral(size);
+ const size = process.argv.slice(2);
+a = Number(size);
+if (a<11 && a>1){
+   createEquilateral(a);
+}
+else{
+   console.log("Please enter number between 2 to 10")
+}
