c284ad5222245ce2604c054e40ce709757b7ddcb
Singh-Parmeet <parmeet.singh@successive.tech>
2021-09-15 15:40:58 +0530

    minor changes

--- src/controllers/user/Controller.ts
+++ src/controllers/user/Controller.ts
@@ -2,12 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
 import config from '../../config/configuration';
 import UserRepository from '../../repositories/user/UserRepository';
-const userRepository = new UserRepository();
+
 class User {
     // Read-All
+    userRepository = new UserRepository();
     getAll = async (req: Request, res: Response, next: NextFunction) => {
         try {
-          const data = await userRepository.find({});
+          const data = await this.userRepository.find({});
           console.log('data', typeof data);
           res.status(200).json({ data, count: data.length });
         } catch (err) {
@@ -17,29 +18,29 @@ class User {
     create = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const {name, role, password, email} = req.body;
-            const data = await userRepository.create({name, role, password, email});
+            const data = await this.userRepository.create({name, role, password, email});
             console.log('data', typeof data);
-            res.status(200).json({ data, count: userRepository.count });
+            res.status(200).json({ data, count: this.userRepository.count });
         } catch (err) {
             console.log(err);
         }
     }
     update = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const {name, role, password, email} = req.body;
-            const data = await userRepository.update({name, role, password, email});
+            const {id} = req.body;
+            const data = await this.userRepository.update({id});
             console.log('data', typeof data);
-            res.status(200).json({ data, count: userRepository.count });
+            res.status(200).json({ data, count: this.userRepository.count });
         } catch (err) {
             console.log(err);
         }
     }
     delete = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const {name, role, password, email} = req.body;
-            const data = await userRepository.delete({name, role, password, email});
+            const {id} = req.body;
+            const data = await this.userRepository.delete({id});
             console.log('data', typeof data);
-            res.status(200).json({ data, count: userRepository.count });
+            res.status(200).json({ data, count: this.userRepository.count });
         } catch (err) {
             console.log(err);
         }
