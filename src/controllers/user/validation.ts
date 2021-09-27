da03057d54f2f5382e39dcb49c6110abf5872306
Singh-Parmeet <parmeet.singh@successive.tech>
2021-09-27 18:26:48 +0530

    minor changes

--- src/controllers/user/validation.ts
+++ src/controllers/user/validation.ts
@@ -50,14 +50,12 @@ export default Object.freeze({
     get: {
         skip: {
             exists: true,
-            default: 0,
             number: true,
             in: [param],
             errorMessage: 'Skip is invalid',
         },
         limit: {
             exist: true,
-            default: 10,
             number: true,
             in: [param],
             errorMessage: 'Limit is invalid',
